7caf6305d7e759a5a99363e2ce1f243c910ec763
niksmac <nikhil@lightrains.com>
2018-09-20 23:30:46 +0530

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,65 @@
+(function($) {
+  "use strict";
+
+  // var input = $(".validate-input .input100");
+  //
+  // $(".validate-form").on("submit", function() {
+  //   var check = true;
+  //
+  //   for (var i = 0; i < input.length; i++) {
+  //     if (validate(input[i]) == false) {
+  //       showValidate(input[i]);
+  //       check = false;
+  //     }
+  //   }
+  //
+  //   return check;
+  // });
+  //
+  // $(".validate-form .input100").each(function() {
+  //   $(this).focus(function() {
+  //     hideValidate(this);
+  //   });
+  // });
+  //
+  // function validate(input) {
+  //   if ($(input).attr("type") == "email" || $(input).attr("name") == "email") {
+  //     if (
+  //       $(input)
+  //         .val()
+  //         .trim()
+  //         .match(
+  //           /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/
+  //         ) == null
+  //     ) {
+  //       return false;
+  //     }
+  //   } else {
+  //     if (
+  //       $(input)
+  //         .val()
+  //         .trim() == ""
+  //     ) {
+  //       return false;
+  //     }
+  //   }
+  // }
+  //
+  // function showValidate(input) {
+  //   var thisAlert = $(input).parent();
+  //
+  //   $(thisAlert).addClass("alert-validate");
+  // }
+  //
+  // function hideValidate(input) {
+  //   var thisAlert = $(input).parent();
+  //
+  //   $(thisAlert).removeClass("alert-validate");
+  // }
+
+  var image = new Array();
+  var image = ["bg01.jpg", "bg02.jpg", "bg03.jpg", "bg04.jpg", "bg05.jpg"];
+  var size = image.length;
+  var x = Math.floor(size * Math.random());
+  $("#random").css("background-image", "url(images/" + image[x] + ")");
+})(jQuery);
